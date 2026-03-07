1bad5284e1f6318da731deb6d1f9fc21d837a4a8
Denis Tatarnikov <strong2much@gmail.com>
2026-03-07 23:01:53 +0700

    Add missed class

---
+++ src/Veldrid/MTL/MTLSwapchainTexture.cs
@@ -0,0 +1,42 @@
+﻿using Veldrid.MetalBindings;
+
+namespace Veldrid.MTL
+{
+    internal class MTLSwapchainTexture : MTLTexture
+    {
+        private MetalBindings.MTLTexture _deviceTexture;
+        private uint _width;
+        private uint _height;
+        private MTLPixelFormat _mtlPixelFormat;
+
+        public override MetalBindings.MTLTexture DeviceTexture => _deviceTexture;
+
+        public override MTLPixelFormat MTLPixelFormat => _mtlPixelFormat;
+
+        public override MTLTextureType MTLTextureType => MTLTextureType.Type2D;
+
+        public override uint Width => _width;
+
+        public override uint Height => _height;
+
+        public override uint Depth => 1;
+
+        public override uint ArrayLayers => 1;
+
+        public override uint MipLevels => 1;
+
+        public override TextureUsage Usage => TextureUsage.RenderTarget;
+
+        public override TextureType Type => TextureType.Texture2D;
+
+        public override TextureSampleCount SampleCount => TextureSampleCount.Count1;
+        
+        public void SetDrawable(CAMetalDrawable drawable, CGSize size, PixelFormat format)
+        {
+            _deviceTexture = drawable.texture;
+            _width = (uint)size.width;
+            _height = (uint)size.height;
+            _mtlPixelFormat = MTLFormats.VdToMTLPixelFormat(Format, false);
+        }
+    }
+}
